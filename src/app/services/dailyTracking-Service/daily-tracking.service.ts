e082048feaea195bedcf269a2a7511cecd207760
marcapedoh <adralabel@gmail.com>
2025-07-23 08:14:40 +0200

    new version

---
+++ src/app/services/dailyTracking-Service/daily-tracking.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DailyTrackingService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllSnapshot(snapshotDate: string, page: number = 0, size: number = 10) {
+    const params = {
+      snapshotDate: snapshotDate,
+      page: page,
+      size: size
+    };
+
+    return this.httpClient.get("http://localhost:8080/agent-tracking/api/data/all-snapshots", {
+      params: params
+    });
+
+  }
+
+  getAllZone() {
+    return this.httpClient.get("http://localhost:8080/agent-tracking/api/data/zones")
+  }
+  getSubZonesPerZoneName(zoneName: string) {
+    return this.httpClient.get(`http://localhost:8080/agent-tracking/api/data/zones/${zoneName}/subzones/names`)
+  }
+}
